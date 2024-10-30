e0308944ad664b2a24b3fa88e2a2a54fb9915d9c
睿晟 沈 <eric960814@gmail.com>
2024-10-30 09:26:47 +0800

    wpf小畫家 加入專案檔案。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Point start, dest;
+        Color strokeColor = Colors.Black;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
